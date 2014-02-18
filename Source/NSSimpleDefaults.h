9b4fb406976fe7f49452c2e40c3baf00170f0c97
Cacauu <achternbosch@me.com>
2014-02-18 08:48:50 +0100

    Add source

---
+++ Source/NSSimpleDefaults.h
@@ -0,0 +1,17 @@
+//
+//  NSSimpleDefaults.h
+//  Magazine
+//
+//  Created by Yannik Achternbosch on 18/02/14.
+//  Copyright (c) 2014 Yannik Achternbosch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSSimpleDefaults : NSObject
+
++(void)setObject:(id)object forKey:(NSString *)key;
+
++(void)getObjectForKey:(NSString *)key;
+
+@end
---
+++ Source/NSSimpleDefaults.m
@@ -0,0 +1,23 @@
+//
+//  NSSimpleDefaults.m
+//  Magazine
+//
+//  Created by Yannik Achternbosch on 18/02/14.
+//  Copyright (c) 2014 Yannik Achternbosch. All rights reserved.
+//
+
+#import "NSSimpleDefaults.h"
+
+@implementation NSSimpleDefaults
+
++(void)setObject:(id)object forKey:(NSString *)key {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:object forKey:key];
+}
+
++(void)getObjectForKey:(NSString *)key {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults valueForKey:key];
+}
+
+@end
